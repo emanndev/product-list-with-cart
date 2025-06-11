47a5d22642a021014696b9ede1da4973bea971bd
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-06-11 16:49:23 +0000

    Cart component implemented

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, Input, PipeTransform, Pipe } from '@angular/core';
-import { Dessert } from '../../../model/dessert.interface';
-import { MainLogicService } from '../../services/main-logic.service';
-import { map, Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { CartServiceService } from '../../services/cart.service';
 import { CartItem } from '../../../model/cartItem.interface';
 import { CommonModule } from '@angular/common';
+import { Dessert } from '../../../model/dessert.interface';
 
 @Component({
   selector: 'app-cart',
@@ -17,22 +17,14 @@ export class CartComponent implements OnInit {
   cartItems$!: Observable<CartItem[]>;
   itemCount$!: Observable<number>;
   total$!: Observable<number>;
-
-  // constructor(private mainLogicService: MainLogicService) {}
+  showModal = false;
 
   constructor(private cartService: CartServiceService) {}
 
   ngOnInit(): void {
-    // this.desserts = this.mainLogicService.getDesserts();
     this.cartItems$ = this.cartService.cartItems$;
-    this.itemCount$ = this.cartItems$.pipe(
-      map((items) => items.reduce((c, i) => c + i.quantity, 0))
-    );
-    this.total$ = this.cartItems$.pipe(
-      map((items) =>
-        items.reduce((sum, item) => sum + item.product.price * item.quantity, 0)
-      )
-    );
+    this.itemCount$ = this.cartService.totalQuantity$;
+    this.total$ = this.cartService.totalPrice$;
   }
 
   removeItem(product: Dessert): void {
@@ -40,6 +32,11 @@ export class CartComponent implements OnInit {
   }
 
   placeOrder(): void {
+    this.showModal = true;
     console.log('Order placed!', this.cartService.cartItems$);
+  }
+
+  closeModal(): void {
+    this.showModal = false;
   }
 }
